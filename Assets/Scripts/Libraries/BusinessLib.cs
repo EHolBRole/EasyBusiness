e9f22477a56c49f349058e064a0179a88b767d3e
EHolBRole <52851286+EHolBRole@users.noreply.github.com>
2022-11-22 00:15:53 +0300

    Update BusinessLib.cs

--- Assets/Scripts/Libraries/BusinessLib.cs
+++ Assets/Scripts/Libraries/BusinessLib.cs
@@ -10,6 +10,7 @@ namespace Libraries
         public static Business smallCafe = new Business("Small Cafe", 600, 200, 5, 100, null, Business.BusinessTier.BaseTier, null);
         public static Business smallSushiRestaraunt = new Business("Small Sushi Restaraunt", 5000, 800, 15, 100, null, Business.BusinessTier.FirstTier, null);
         // Create other Cafe-Root businesses
+
         public static List<Business> allBusinessList = new List<Business>() { smallCafe, smallSushiRestaraunt};
     }
 }
